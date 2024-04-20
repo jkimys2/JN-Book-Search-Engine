32a1f2bbeded07b3d19751d4a794f97d36aa59ec
Jordan Newhouse <jkimys2@gmail.com>
2024-04-20 17:05:16 -0400

    typedef file update

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,26 @@
 const gql = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+}
+
+type Auth {
+    
+}
+`;
 
 module.exports = typeDefs;

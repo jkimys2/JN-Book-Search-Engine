953217f0d6305b771252d4820e9f190abe071fbb
Jordan Newhouse <jkimys2@gmail.com>
2024-04-20 14:44:56 -0400

    resolver file update

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+// Import files
+const { User } = require("../models/User");
+const { signToken, AuthenticanError } = require("../utils/auth");
+
+const resolvers = {
+
+}
+
+module.exports = resolvers;
